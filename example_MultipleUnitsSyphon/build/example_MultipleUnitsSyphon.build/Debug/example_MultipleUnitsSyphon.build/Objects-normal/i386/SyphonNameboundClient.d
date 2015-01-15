6ebb856f82b37960fddb9e6b8ae20b7e3a4039c9
David Haylock <david.haylock@watershed.co.uk>
2015-01-15 12:56:38 +0000

    - Small updates to xprojs

--- example_MultipleUnitsSyphon/build/example_MultipleUnitsSyphon.build/Debug/example_MultipleUnitsSyphon.build/Objects-normal/i386/SyphonNameboundClient.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/davidhaylock/Desktop/of_v0.8.4_osx_release/addons/ofxSyphon/libs/Syphon/src/SyphonNameboundClient.m \
-  ../../../addons/ofxSyphon/libs/Syphon/src/SyphonNameboundClient.h \
-  /Users/davidhaylock/Desktop/of_v0.8.1_osx_release/addons/ofxSyphon/libs/Syphon/lib/osx/Syphon.framework/Headers/Syphon.h \
-  /Users/davidhaylock/Desktop/of_v0.8.1_osx_release/addons/ofxSyphon/libs/Syphon/lib/osx/Syphon.framework/Headers/SyphonServerDirectory.h \
-  /Users/davidhaylock/Desktop/of_v0.8.1_osx_release/addons/ofxSyphon/libs/Syphon/lib/osx/Syphon.framework/Headers/SyphonServer.h \
-  /Users/davidhaylock/Desktop/of_v0.8.1_osx_release/addons/ofxSyphon/libs/Syphon/lib/osx/Syphon.framework/Headers/SyphonClient.h \
-  /Users/davidhaylock/Desktop/of_v0.8.1_osx_release/addons/ofxSyphon/libs/Syphon/lib/osx/Syphon.framework/Headers/SyphonImage.h
