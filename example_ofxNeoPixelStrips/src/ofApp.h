feefae0d297e4c9bc92df165b19f22eaa3673160
DHaylock <david.haylock@watershed.co.uk>
2016-02-09 03:56:03 +0000

    - Added: example for strips

--- example_ofxNeoPixelStrips/src/ofApp.h
+++ example_ofxNeoPixelStrips/src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxOPC.h"
 
 class ofApp : public ofBaseApp{
 
@@ -8,17 +9,16 @@ class ofApp : public ofBaseApp{
 		void setup();
 		void update();
 		void draw();
-
 		void keyPressed(int key);
 		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+        void exit();
+
+        ofxOPC opcClient;
+        ofxNeoPixelStrip stick;
+        ofxNeoPixelStrip strip40px;
+        ofxNeoPixelStrip strip60px;
+        ofxNeoPixelStrip strip144px;
+
+        int effect;
+        bool hide;
 };
