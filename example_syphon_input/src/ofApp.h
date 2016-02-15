1cbd418ce7008411b1644856e4003b6af63a8a28
DHaylock <david.haylock@watershed.co.uk>
2016-02-15 22:21:47 +0000

    - Added: New Example using Syphon

---
+++ example_syphon_input/src/ofApp.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOPC.h"
+#include "ofxSyphon.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void exit();
+    
+        void serverAnnounced(ofxSyphonServerDirectoryEventArgs &arg);
+        void serverUpdated(ofxSyphonServerDirectoryEventArgs &args);
+        void serverRetired(ofxSyphonServerDirectoryEventArgs &arg);
+    
+        ofxOPC opcClient;
+    	ofxSyphonClient syphonClient;
+        ofxSyphonServerDirectory dir;
+        int directoryId;
+
+        ofxNeoPixelStrip leftStrip;
+        ofxNeoPixelStrip rightStrip;
+        ofxNeoPixelRing centerRing;
+        ofxNeoPixelRing leftRing;
+        ofxNeoPixelRing rightRing;
+        ofxNeoPixelGrid centerGrid;
+    
+        int effect;
+        bool hide;
+        bool syphonActive;
+};
